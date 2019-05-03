48a9ab3344cb5684f85cf32284ebbfd2dc25b8fb
Joel Auterson <joel@improbable.io>
2019-05-03 14:48:12 +0100

    Add CI

---
+++ ci/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# https://explainshell.com
+set -euo pipefail
+[[ -n "${DEBUG-}" ]] && set -x
+cd "$(dirname "$0")/../"
+
+pushd services/csharp
+dotnet restore --no-cache
+dotnet build
+
+popd
