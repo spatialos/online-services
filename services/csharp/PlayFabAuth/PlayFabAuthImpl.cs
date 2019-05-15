c42918ee87c1a39c1c49986f13723e71137f016a
Joel Auterson <joel@improbable.io>
2019-05-15 16:53:41 +0100

    Add PlayFab auth service

---
+++ services/csharp/PlayFabAuth/PlayFabAuthImpl.cs
@@ -0,0 +1,65 @@
+﻿using Improbable.OnlineServices.Proto.Auth.PlayFab;
+using Grpc.Core;
+using PlayFab;
+using PlayFab.ServerModels;
+using System.Threading.Tasks;
+using System;
+using Serilog;
+using Improbable.SpatialOS.PlayerAuth.V2Alpha1;
+
+namespace PlayFabAuth
+{
+    public class PlayFabAuthImpl : AuthService.AuthServiceBase
+    {
+        private readonly string _project;
+        private readonly PlayerAuthServiceClient _authServiceClient;
+
+        public PlayFabAuthImpl(string project, PlayerAuthServiceClient authServiceClient)
+        {
+            _project = project;
+            _authServiceClient = authServiceClient;
+        }
+
+        public override Task<ExchangePlayFabTokenResponse> ExchangePlayFabToken(ExchangePlayFabTokenRequest request,
+            ServerCallContext context)
+        {
+            UserAccountInfo userInfo;
+
+            try
+            {
+                var authenticateTask = PlayFabServerAPI.AuthenticateSessionTicketAsync(
+                    new AuthenticateSessionTicketRequest
+                    {
+                        SessionTicket = request.PlayfabToken,
+                    });
+                authenticateTask.Wait();
+                userInfo = authenticateTask.GetAwaiter().GetResult().Result.UserInfo;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Failed to authenticate PlayFab ticket");
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Failed to authenticate PlayFab ticket"));
+            }
+
+            try
+            {
+                var playerIdentityToken = _authServiceClient.CreatePlayerIdentityToken(
+                    new CreatePlayerIdentityTokenRequest
+                    {
+                        PlayerIdentifier = userInfo.PlayFabId,
+                        Provider = "playfab",
+                        ProjectName = _project
+                    }
+                );
+                return Task.FromResult(new ExchangePlayFabTokenResponse
+                    {PlayerIdentityToken = playerIdentityToken.PlayerIdentityToken});
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, $"Failed to create player identity token for {userInfo.PlayFabId}");
+                throw new RpcException(new Status(StatusCode.Internal,
+                    $"Failed to create player identity token for {userInfo.PlayFabId}"));
+            }
+        }
+    }
+}
---
+++ services/csharp/PlayFabAuth/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommandLine;
+using Improbable.OnlineServices.Base.Server;
+using Improbable.OnlineServices.Proto.Auth.PlayFab;
+using Improbable.SpatialOS.Platform.Common;
+using Improbable.SpatialOS.PlayerAuth.V2Alpha1;
+using Mono.Unix;
+using PlayFab;
+using Mono.Unix.Native;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+namespace PlayFabAuth
+{
+    public class PlayFabAuthArguments : CommandLineArgs
+    {
+        [Option("spatial_project", HelpText = "Spatial project name", Required = true)]
+        public string SpatialProject { get; set; }
+
+        [Option("playfab_secret_key", HelpText = "PlayFab developer secret key", Required = true)]
+        public string PlayFabSecretKey { get; set; }
+
+        [Option("playfab_title_id", HelpText = "PlayFab title ID", Required = true)]
+        public string PlayFabTitleId { get; set; }
+
+        [Option("spatial_refresh_token", HelpText = "Refresh Token to communicate with SpatialOS", Required = true)]
+        public string RefreshToken { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<PlayFabAuthArguments>(args)
+                .WithParsed(parsedArgs =>
+                {
+                    Log.Logger = new LoggerConfiguration()
+                        .WriteTo.Console(new RenderedCompactJsonFormatter())
+                        .Enrich.FromLogContext()
+                        .CreateLogger();
+
+                    PlayFabSettings.DeveloperSecretKey = parsedArgs.PlayFabSecretKey;
+                    PlayFabSettings.TitleId = parsedArgs.PlayFabTitleId;
+
+                    var server = GrpcBaseServer.Build(parsedArgs);
+                    server.AddService(AuthService.BindService(
+                        new PlayFabAuthImpl(
+                            parsedArgs.SpatialProject,
+                            PlayerAuthServiceClient.Create(
+                                credentials: new PlatformRefreshTokenCredential(parsedArgs.RefreshToken))
+                        )
+                    ));
+                    var serverTask = new Task(() => server.Start());
+                    var unixSignalTask = new Task<int>(() =>
+                        UnixSignal.WaitAny(new[] {new UnixSignal(Signum.SIGINT), new UnixSignal(Signum.SIGTERM)}));
+
+                    serverTask.Start();
+                    Log.Information("Matcher started up");
+                    unixSignalTask.Start();
+                    Task.WaitAny(serverTask, unixSignalTask);
+                    if (unixSignalTask.IsCompleted)
+                    {
+                        Log.Information($"Received UNIX signal {unixSignalTask.Result}");
+                        Log.Information("Server shutting down...");
+                        server.Shutdown();
+                        serverTask.Wait(TimeSpan.FromSeconds(10));
+                        Log.Information("Server stopped cleanly");
+                    }
+                    else
+                    {
+                        /* The matcher task has completed; we can just exit. */
+                        Log.Information("The matcher has stopped itself or encountered an unhandled exception.");
+                    }
+
+                    Environment.Exit(0);
+                });
+        }
+    }
+}
