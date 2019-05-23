b209c8dea2113e36931be5e3bc27d008a6b46e60
Ben Clive <benclive@improbable.io>
2019-05-23 14:10:59 +0100

    Move files into docker container

--- ci/format.sh
+++ ci/format.sh
@@ -1,6 +1,6 @@
 cd $(dirname "${BASH_SOURCE[0]}")
 
-docker build . -f ./Dockerfile -t online-services-ci:latest
+docker build docker/online-services-ci -f ./docker/online-services-ci/Dockerfile -t online-services-ci:latest
 
 cd ../
 docker run -e LOCAL_USER_ID=$(id -u) -v $(pwd):/code online-services-ci:latest /bin/bash -c "dotnet --info >/dev/null; dotnet format -w /code/services/csharp --check --dry-run"
