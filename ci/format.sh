8bc0254c0f55e030d5aef672331989be857c4add
Ben Clive <benclive@improbable.io>
2019-05-23 15:24:58 +0100

    Format fixes errors it finds

--- ci/format.sh
+++ ci/format.sh
@@ -3,4 +3,4 @@ cd $(dirname "${BASH_SOURCE[0]}")
 docker build docker/online-services-ci -f ./docker/online-services-ci/Dockerfile -t online-services-ci:latest
 
 cd ../
-docker run -e LOCAL_USER_ID=$(id -u) -v $(pwd):/code online-services-ci:latest /bin/bash -c "dotnet --info >/dev/null; dotnet format -w /code/services/csharp --check --dry-run"
+docker run -e LOCAL_USER_ID=$(id -u) -v $(pwd):/code online-services-ci:latest /bin/bash -c "dotnet format -w /code/services/csharp --check"
