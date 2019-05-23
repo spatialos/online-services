791e0af52c96fb7dc4f55776067b82ec68b29204
Ben Clive <benclive@improbable.io>
2019-05-23 14:01:36 +0100

    Correcting permissions for formatter

--- ci/format.sh
+++ ci/format.sh
@@ -3,4 +3,4 @@ cd $(dirname "${BASH_SOURCE[0]}")
 docker build . -f ./Dockerfile -t online-services-ci:latest
 
 cd ../
-docker run -v $(pwd):/code online-services-ci:latest  /bin/bash  -c "dotnet format -w /code/services/csharp --check"
+docker run -e LOCAL_USER_ID=$(id -u) -v $(pwd):/code online-services-ci:latest /bin/bash -c "dotnet --info >/dev/null; dotnet format -w /code/services/csharp --check --dry-run"
