7d31faa0b02b9304c05b9142ea858ecaa1c0a43d
Joel Auterson <joel@improbable.io>
2019-06-10 18:07:37 +0100

    update terraform config for endpoints/http

--- services/terraform/variables.tf
+++ services/terraform/variables.tf
@@ -4,7 +4,11 @@
 # The Google Cloud project ID (not display name).
 variable "gcloud_project" {}
 
-# A zone (not a region); pick one from: https://cloud.google.com/compute/docs/regions-zones/
+# A gcloud region. Pick one from: https://cloud.google.com/compute/docs/regions-zones/
+variable "gcloud_region" {}
+
+# A gcloud zone. Must be within your specified region!
+# Pick one from: https://cloud.google.com/compute/docs/regions-zones/
 variable "gcloud_zone" {}
 
 variable "k8s_cluster_name" {}
