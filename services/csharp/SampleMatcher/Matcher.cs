80dc6bb2f7d4de50e7317d3c26ebc23b01a84f5b
benclive <36448794+benclive@users.noreply.github.com>
2019-06-11 11:02:50 +0100

    SampleMatcher marks used deployments on match (#15)
    * Add an in-use tag as well as removing ready tag.

--- services/csharp/SampleMatcher/Matcher.cs
+++ services/csharp/SampleMatcher/Matcher.cs
@@ -12,6 +12,8 @@ namespace Improbable.OnlineServices.SampleMatcher
     {
         private const int TickMs = 200;
         private readonly string _project;
+        private readonly string ReadyTag = "ready"; // This should be the same tag a DeploymentPool looks for.
+        private readonly string InUseTag = "in-use";
         private RepeatedField<WaitingParty> _waitingParties;
 
         public Matcher()
@@ -50,6 +52,7 @@ namespace Improbable.OnlineServices.SampleMatcher
                                 Result = Assignment.Types.Result.Matched,
                                 Party = party.Party
                             });
+                            MarkDeploymentAsInUse(deploymentServiceClient, deployment);
                             gatewayClient.AssignDeployments(assignRequest);
                             _waitingParties.Remove(party);
                         }
@@ -101,9 +104,11 @@ namespace Improbable.OnlineServices.SampleMatcher
                 .ListDeployments(new ListDeploymentsRequest
                 {
                     ProjectName = _project,
+                    DeploymentStoppedStatusFilter = ListDeploymentsRequest.Types.DeploymentStoppedStatusFilter.NotStoppedDeployments,
+                    View = ViewType.Basic
                 })
                 .TakeWhile(d => d.Status == Deployment.Types.Status.Running)
-                .FirstOrDefault(d => d.Tag.Contains(tag));
+                .FirstOrDefault(d => d.Tag.Contains(tag) && d.Tag.Contains(ReadyTag));
         }
 
         protected override void DoShutdown(GatewayInternalService.GatewayInternalServiceClient gatewayClient,
@@ -114,6 +119,14 @@ namespace Improbable.OnlineServices.SampleMatcher
             {
                 AssignPartyAsError(gatewayClient, party);
             }
+        }
+
+        private void MarkDeploymentAsInUse(DeploymentServiceClient dplClient, Deployment dpl)
+        {
+            dpl.Tag.Remove(ReadyTag);
+            dpl.Tag.Add(InUseTag);
+            var req = new UpdateDeploymentRequest { Deployment = dpl };
+            dplClient.UpdateDeployment(req);
         }
     }
 }
