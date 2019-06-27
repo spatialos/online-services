a5c41499ee6a949653fc2bd78f86eeef15b294e6
Ben Clive <benclive@improbable.io>
2019-06-27 13:52:54 +0100

    Formatting

--- services/csharp/DeploymentPool/DeploymentPool.cs
+++ services/csharp/DeploymentPool/DeploymentPool.cs
@@ -83,7 +83,7 @@ namespace DeploymentPool
                     var retrySeconds = 2 ^ retries;
                     Log.Logger.Warning("Exception encountered during iteration: Retrying in {s}. Error was {e}", retrySeconds, e);
                     await Task.Delay(TimeSpan.FromSeconds(retrySeconds));
-                    retries = Math.Min(retries+1, 4);
+                    retries = Math.Min(retries + 1, 4);
                 }
             }
 
