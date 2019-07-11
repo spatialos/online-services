2090e7476b34772c04c45887c0e0f00b04b6cbe6
Ben Clive <benclive@improbable.io>
2019-07-11 17:31:25 +0100

    Fix the test I missed

--- services/csharp/Party.Test/GetPartyByPlayerIdShould.cs
+++ services/csharp/Party.Test/GetPartyByPlayerIdShould.cs
@@ -83,7 +83,7 @@ namespace Party.Test
             Assert.AreEqual(_party.MaxMembers, receivedParty.MaxMembers);
             Assert.AreEqual(_party.CurrentPhase.ToString(), receivedParty.CurrentPhase.ToString());
             CollectionAssert.AreEquivalent(_testMetadata, receivedParty.Metadata);
-            Assert.IsEmpty(_party.MemberIdToPit);
+            Assert.IsEmpty(receivedParty.MemberIdToPit);
         }
     }
 }
