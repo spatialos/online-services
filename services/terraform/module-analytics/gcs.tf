de09812565e9d01f330d0fd2ebdbcb55c903fb70
Loek Landzaat <loek@improbable.io>
2019-07-15 18:22:28 +0100

    fixed first few PR comments

--- services/terraform/module-analytics/gcs.tf
+++ services/terraform/module-analytics/gcs.tf
@@ -1,14 +1,12 @@
-# This files creates two GCS buckets.
+# This file creates two GCS buckets.
 
 resource "google_storage_bucket" "analytics_bucket" {
-  # force_destroy = True
   name          = "${var.gcloud_project}-analytics"
   location      = var.gcloud_analytics_bucket_location
   storage_class = "MULTI_REGIONAL"
 }
 
 resource "google_storage_bucket" "functions_bucket" {
-  # force_destroy = True
   name          = "${var.gcloud_project}-cloud-functions"
   location      = var.gcloud_analytics_bucket_location
   storage_class = "MULTI_REGIONAL"
