22632334ca30eb85152c4b85513e5455a70ba7fd
Loek <loek+github@improbable.io>
2019-07-23 16:07:34 +0100

    Analytics terraform

---
+++ services/terraform/module-analytics/archive.tf
@@ -0,0 +1,37 @@
+# This file creates a .zip file with everything required to run our Cloud Function.
+
+data "archive_file" "cloud_function_analytics" {
+  type        = "zip"
+  output_path = "${path.module}/../../python/analytics-pipeline/cloud-function-analytics.zip"
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/function/main.py")}"
+    filename = "main.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/requirements/function.txt")}"
+    filename = "requirements.txt"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/__init__.py")}"
+    filename = "common/__init__.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/bigquery.py")}"
+    filename = "common/bigquery.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/bigquery_schema.py")}"
+    filename = "common/bigquery_schema.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/functions.py")}"
+    filename = "common/functions.py"
+  }
+
+}
---
+++ services/terraform/versions.tf
@@ -0,0 +1,5 @@
+# This file defines the required Terraform version.
+
+terraform {
+  required_version = ">= 0.12"
+}
