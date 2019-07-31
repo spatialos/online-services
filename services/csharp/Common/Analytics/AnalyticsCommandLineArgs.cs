ff9308f3fc91596d227d9953a60820423798129e
Ashley Davies <ashleydavieslyons@improbable.io>
2019-07-31 12:10:00 +0100

    Base for analytics instrumentation system (#57)
    * Begin implementing classes for analytics reporting

---
+++ services/csharp/Common/Analytics/AnalyticsCommandLineArgs.cs
@@ -0,0 +1,17 @@
+using CommandLine;
+
+namespace Improbable.OnlineServices.Common.Analytics
+{
+    public class AnalyticsCommandLineArgs
+    {
+        public const string EndpointName = "analytics.endpoint";
+        public const string AllowInsecureEndpointName = "analytics.allow-insecure-endpoint";
+
+        [Option(EndpointName, HelpText =
+            "Endpoint for analytics to be sent to. If not provided, then analytics are disabled")]
+        public string Endpoint { get; set; }
+
+        [Option(AllowInsecureEndpointName, Default = false, HelpText = "If set, allows http URLs for the endpoint")]
+        public bool AllowInsecureEndpoints { get; set; }
+    }
+}
