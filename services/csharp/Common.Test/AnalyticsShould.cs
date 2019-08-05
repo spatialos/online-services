0dce3be14a3475787185dbd7001141961c04d796
Ashley Davies <ashleydavieslyons@improbable.io>
2019-08-05 15:34:13 +0100

    Analytics config (#61)
    * Initial analytics config functionality
    
    * Add unit tests for analytics config
    
    * Fetch category from config
    
    * Config file command line argument and migrate to a proper builder class

--- services/csharp/Common.Test/AnalyticsShould.cs
+++ services/csharp/Common.Test/AnalyticsShould.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Improbable.OnlineServices.Common.Analytics;
+using Improbable.OnlineServices.Common.Analytics.Config;
 using Moq;
 using Moq.Protected;
 using Newtonsoft.Json;
@@ -16,6 +16,13 @@ namespace Improbable.OnlineServices.Common.Test
     public class AnalyticsShould
     {
         private Mock<HttpMessageHandler> _messageHandlerMock;
+        private const string SourceVal = "event_source_value";
+        private const string ClassVal = "event_class_value";
+        private const string TypeVal = "event_type_value";
+        private const string KeyVal = "gcp_key_value";
+
+        // The default event category we expect when one isn't provided by a config file
+        private const string DefaultEventCategory = "cold";
 
         [SetUp]
         public void Setup()
@@ -37,24 +44,28 @@ namespace Improbable.OnlineServices.Common.Test
         [Test]
         public void BuildNullByDefault()
         {
-            Assert.IsInstanceOf<NullAnalyticsSender>(AnalyticsSender.Build(new string[] { },
-                AnalyticsEnvironment.Development, ""));
+            Assert.IsInstanceOf<NullAnalyticsSender>(
+                new AnalyticsSenderBuilder(AnalyticsEnvironment.Development, KeyVal, SourceVal).Build()
+            );
         }
 
         [Test]
         public void BuildRealAnalyticsSenderIfProvidedWithEndpoint()
         {
-            Assert.IsInstanceOf<AnalyticsSender>(AnalyticsSender.Build(
-                new[] { $"--{AnalyticsCommandLineArgs.EndpointName}", "https://example.com/" },
-                AnalyticsEnvironment.Development, ""));
+            Assert.IsInstanceOf<AnalyticsSender>(
+                new AnalyticsSenderBuilder(AnalyticsEnvironment.Development, KeyVal, SourceVal)
+                    .WithCommandLineArgs($"--{AnalyticsCommandLineArgs.EndpointName}", "https://example.com/")
+                    .Build()
+            );
         }
 
         [Test]
         public void FailToBuildIfHttpIsNotUsedWithoutInsecureEnabled()
         {
             ArgumentException ex = Assert.Throws<ArgumentException>(
-                () => AnalyticsSender.Build(new[] { $"--{AnalyticsCommandLineArgs.EndpointName}", "http://example.com/" },
-                    AnalyticsEnvironment.Development, ""));
+                () => new AnalyticsSenderBuilder(AnalyticsEnvironment.Development, KeyVal, SourceVal)
+                    .WithCommandLineArgs($"--{AnalyticsCommandLineArgs.EndpointName}", "http://example.com/").Build()
+            );
 
             Assert.That(ex.Message, Contains.Substring("uses http, but only https is allowed"));
         }
@@ -62,16 +73,13 @@ namespace Improbable.OnlineServices.Common.Test
         [Test]
         public void AllowsHttpIfInsecureEndpointsEnabled()
         {
-            Assert.IsInstanceOf<AnalyticsSender>(AnalyticsSender.Build(
-                new[] { $"--{AnalyticsCommandLineArgs.EndpointName}", "http://example.com/",
-                    $"--{AnalyticsCommandLineArgs.AllowInsecureEndpointName}" },
-                AnalyticsEnvironment.Development, ""));
+            Assert.IsInstanceOf<AnalyticsSender>(
+                new AnalyticsSenderBuilder(AnalyticsEnvironment.Development, KeyVal, SourceVal)
+                    .WithCommandLineArgs($"--{AnalyticsCommandLineArgs.EndpointName}", "http://example.com/",
+                        $"--{AnalyticsCommandLineArgs.AllowInsecureEndpointName}")
+                    .Build()
+            );
         }
-
-        private const string SourceVal = "event_source_value";
-        private const string ClassVal = "event_class_value";
-        private const string TypeVal = "event_type_value";
-        private const string KeyVal = "fakeKey";
 
         private bool ExpectedMessage(HttpRequestMessage request)
         {
@@ -83,12 +91,11 @@ namespace Improbable.OnlineServices.Common.Test
                 dynamic content = JsonConvert.DeserializeObject(messageContent.ReadAsStringAsync().Result);
 
                 // TODO: Test versioning when it is added
-                Assert.AreEqual(content.eventEnvironment.Value,
-                    development);
-                Assert.AreEqual(content.eventIndex.Value, "0");
-                Assert.AreEqual(content.eventSource.Value, SourceVal);
-                Assert.AreEqual(content.eventClass.Value, ClassVal);
-                Assert.AreEqual(content.eventType.Value, TypeVal);
+                Assert.AreEqual(development, content.eventEnvironment.Value);
+                Assert.AreEqual("0", content.eventIndex.Value);
+                Assert.AreEqual(SourceVal, content.eventSource.Value);
+                Assert.AreEqual(ClassVal, content.eventClass.Value);
+                Assert.AreEqual(TypeVal, content.eventType.Value);
                 Assert.True(Guid.TryParse(content.sessionId.Value, out Guid _));
 
                 // Check the timestamp is within 5 seconds of now (i.e. roughly correct)
@@ -102,11 +109,10 @@ namespace Improbable.OnlineServices.Common.Test
             }
 
             var queryCollection = request.RequestUri.ParseQueryString();
-            Assert.AreEqual(queryCollection["key"], KeyVal);
-            Assert.AreEqual(queryCollection["analytics_environment"], development);
-            // TODO: Update with real category
-            Assert.AreEqual(queryCollection["event_category"], "");
-            Assert.True(Guid.TryParse(queryCollection["session_id"], out Guid _));
+            Assert.AreEqual(KeyVal, queryCollection["key"]);
+            Assert.AreEqual(development, queryCollection["analytics_environment"]);
+            Assert.AreEqual(DefaultEventCategory, queryCollection["event_category"]);
+            Assert.True(Guid.TryParse((string) queryCollection["session_id"], out Guid _));
 
             return request.Method == HttpMethod.Post;
         }
@@ -115,17 +121,66 @@ namespace Improbable.OnlineServices.Common.Test
         public void SendAnalyticEventsToHttpsEndpoint()
         {
             HttpClient client = new HttpClient(_messageHandlerMock.Object);
-            AnalyticsSender.Build(new[] { $"--{AnalyticsCommandLineArgs.EndpointName}", "https://example.com/" },
-                    AnalyticsEnvironment.Development, KeyVal, SourceVal, client)
+            new AnalyticsSenderBuilder(AnalyticsEnvironment.Development, KeyVal, SourceVal)
+                .WithCommandLineArgs($"--{AnalyticsCommandLineArgs.EndpointName}", "https://example.com/")
+                .With(client)
+                .Build()
                 .Send(ClassVal, TypeVal, new Dictionary<string, string>
                 {
-                    {"dogs", "excellent"}
+                    { "dogs", "excellent" }
                 });
 
-            // TODO: Verify contents of the message
             _messageHandlerMock.Protected().Verify("SendAsync", Times.Exactly(1),
                 ItExpr.Is<HttpRequestMessage>(req => ExpectedMessage(req)),
                 ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Test]
+        public void FallBackToDefaultConfigurationGracefully()
+        {
+            AnalyticsConfig config = new AnalyticsConfig("");
+            Assert.AreEqual(config.GetCategory("c", "t"),
+                DefaultEventCategory);
+            Assert.True(config.IsEnabled("c", "t"));
+        }
+
+        private readonly string _configString = @"
+'*':
+  '*':
+    disabled: true
+  'a':
+    category: 'function'
+'b':
+  '*':
+    category: 'function-2'
+  'c':
+    category: 'function-3'
+";
+
+        [Test]
+        public void HandleConfigPrecedenceRulesCorrectly()
+        {
+            AnalyticsConfig config = new AnalyticsConfig(_configString);
+
+            // d.e should route to *.* as there is no match
+            Assert.False(config.IsEnabled("d", "e"));
+            Assert.AreEqual(DefaultEventCategory, config.GetCategory("d", "e"));
+
+            // d.a should route to *.a as there is not a better match
+            Assert.True(config.IsEnabled("d", "a"));
+            Assert.AreEqual("function", config.GetCategory("d", "a"));
+
+            // b.d should route to b.* as there is not a better match
+            Assert.True(config.IsEnabled("b", "d"));
+            Assert.AreEqual("function-2", config.GetCategory("b", "d"));
+
+            // b.a should route to b.* as a match on class is preferred on a match on type (i.e. b.* > *.a)
+            Assert.True(config.IsEnabled("b", "a"));
+            Assert.AreEqual("function-2", config.GetCategory("b", "a"));
+
+            // b.c should route to b.c as it is an exact match
+            Assert.True(config.IsEnabled("b", "c"));
+            Assert.AreEqual("function-3", config.GetCategory("b", "c"));
         }
     }
 }
---
+++ services/csharp/Common/Analytics/AnalyicsSenderBuilder.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using CommandLine;
+using Improbable.OnlineServices.Common.Analytics.Config;
+
+namespace Improbable.OnlineServices.Common.Analytics
+{
+    public class AnalyticsSenderBuilder
+    {
+        private readonly AnalyticsEnvironment _environment;
+        private readonly string _gcpKey;
+        private readonly string _eventSource;
+        private AnalyticsConfig _config;
+        private bool _allowUnsafeEndpoints;
+        private HttpClient _httpClient = new HttpClient();
+        private Uri _endpoint;
+
+        private readonly string _insecureProtocolExceptionMessage
+            = $"The endpoint provided uses {{0}}, but only {Uri.UriSchemeHttps} is allowed. " +
+              $"Enable insecure communication with --{AnalyticsCommandLineArgs.AllowInsecureEndpointName}.";
+
+        public AnalyticsSenderBuilder(AnalyticsEnvironment environment, string gcpKey, string eventSource)
+        {
+            _environment = environment;
+            _gcpKey = gcpKey;
+            _eventSource = eventSource;
+        }
+
+        public IAnalyticsSender Build()
+        {
+            _config = _config ?? new AnalyticsConfig();
+
+            if (_endpoint != null)
+            {
+                if (_endpoint.Scheme != Uri.UriSchemeHttps && !_allowUnsafeEndpoints)
+                {
+                    throw new ArgumentException(
+                        string.Format(_insecureProtocolExceptionMessage, _endpoint.Scheme));
+                }
+
+                return new AnalyticsSender(_endpoint, _config, _environment, _gcpKey, _eventSource, _httpClient);
+            }
+
+            return new NullAnalyticsSender();
+        }
+
+        public AnalyticsSenderBuilder With(HttpClient client)
+        {
+            _httpClient = client;
+            return this;
+        }
+
+        public AnalyticsSenderBuilder With(AnalyticsConfig config)
+        {
+            _config = config;
+            return this;
+        }
+
+        public AnalyticsSenderBuilder WithCommandLineArgs(params string[] args)
+        {
+            return WithCommandLineArgs(args.ToList());
+        }
+
+        public AnalyticsSenderBuilder WithCommandLineArgs(IEnumerable<string> args)
+        {
+            Parser.Default.ParseArguments<AnalyticsCommandLineArgs>(args)
+                .WithParsed(async parsedArgs =>
+                {
+                    if (!string.IsNullOrEmpty(parsedArgs.ConfigPath))
+                    {
+                        _config = AnalyticsConfig.FromFile(parsedArgs.ConfigPath);
+                    }
+
+                    if (!string.IsNullOrEmpty(parsedArgs.Endpoint))
+                    {
+                        _endpoint = new Uri(parsedArgs.Endpoint);
+                    }
+
+                    _allowUnsafeEndpoints = parsedArgs.AllowInsecureEndpoints;
+                });
+            return this;
+        }
+    }
+}
