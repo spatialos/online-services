431fd74246674e4200cb5041676f0c7142ffe805
Ashley Davies <ashleydavieslyons@improbable.io>
2019-08-05 15:34:13 +0100

    Analytics config (#61)
    * Initial analytics config functionality
    
    * Add unit tests for analytics config
    
    * Fetch category from config
    
    * Config file command line argument and migrate to a proper builder class

--- services/csharp/Common/Analytics/AnalyticsSender.cs
+++ services/csharp/Common/Analytics/AnalyticsSender.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
-using CommandLine;
+using Improbable.OnlineServices.Common.Analytics.Config;
 using Newtonsoft.Json;
 
 namespace Improbable.OnlineServices.Common.Analytics
@@ -21,7 +22,10 @@ namespace Improbable.OnlineServices.Common.Analytics
 
     public class AnalyticsSender : IAnalyticsSender
     {
+        internal const string DefaultEventCategory = "cold";
+
         private readonly Uri _endpoint;
+        private readonly AnalyticsConfig _config;
         private readonly AnalyticsEnvironment _environment;
         private readonly string _sessionId = Guid.NewGuid().ToString();
         private readonly string _gcpKey;
@@ -30,39 +34,15 @@ namespace Improbable.OnlineServices.Common.Analytics
 
         private long _eventId;
 
-        public static IAnalyticsSender Build(IEnumerable<string> args, AnalyticsEnvironment environment,
-            string gcpKey, string eventSource = "server", HttpClient client = null)
-        {
-            IAnalyticsSender sender = new NullAnalyticsSender();
-
-            Parser.Default.ParseArguments<AnalyticsCommandLineArgs>(args)
-                .WithParsed(parsedArgs =>
-                {
-                    if (parsedArgs.Endpoint != null)
-                    {
-                        sender = new AnalyticsSender(parsedArgs, environment, gcpKey, eventSource,
-                            client ?? new HttpClient());
-                    }
-                });
-
-            return sender;
-        }
-
-        private AnalyticsSender(AnalyticsCommandLineArgs args, AnalyticsEnvironment environment,
+        internal AnalyticsSender(Uri endpoint, AnalyticsConfig config, AnalyticsEnvironment environment,
             string gcpKey, string eventSource, HttpClient httpClient)
         {
+            _endpoint = endpoint;
+            _config = config;
             _environment = environment;
             _gcpKey = gcpKey;
             _eventSource = eventSource;
             _httpClient = httpClient;
-            _endpoint = new Uri(args.Endpoint);
-
-            if (_endpoint.Scheme != Uri.UriSchemeHttps && !args.AllowInsecureEndpoints)
-            {
-                throw new ArgumentException(
-                    $"The endpoint provided uses {_endpoint.Scheme}, but only {Uri.UriSchemeHttps} is allowed. "
-                    + $"Enable insecure communication with --{AnalyticsCommandLineArgs.AllowInsecureEndpointName}.");
-            }
         }
 
         public async Task Send(string eventClass, string eventType, Dictionary<string, string> eventAttributes)
@@ -75,26 +55,26 @@ namespace Improbable.OnlineServices.Common.Analytics
             //   params into JSON?
             var postParams = new Dictionary<string, string>
             {
-                {"eventEnvironment", environment},
-                {"eventIndex", eventId.ToString()},
-                {"eventSource", _eventSource},
-                {"eventClass", eventClass},
-                {"eventType", eventType},
-                {"sessionId", _sessionId},
+                { "eventEnvironment", environment },
+                { "eventIndex", eventId.ToString() },
+                { "eventSource", _eventSource },
+                { "eventClass", eventClass },
+                { "eventType", eventType },
+                { "sessionId", _sessionId },
                 // TODO: Add versioning ability & resolve matching TODO in relevant unit tests
-                {"buildVersion", "v0.0.0"},
-                {"eventTimestamp", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString()},
-                {"eventAttributes", JsonConvert.SerializeObject(eventAttributes)},
+                { "buildVersion", "0.0.0" },
+                { "eventTimestamp", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString() },
+                { "eventAttributes", JsonConvert.SerializeObject(eventAttributes) },
             };
 
             UriBuilder builder = new UriBuilder(_endpoint)
             {
                 Query = DictionaryToQueryString(new Dictionary<string, string>
                 {
-                    {"key", _gcpKey},
-                    {"analytics_environment", environment},
-                    {"event_category", ""},
-                    {"session_id", _sessionId}
+                    { "key", _gcpKey },
+                    { "analytics_environment", environment },
+                    { "event_category", _config.GetCategory(eventClass, eventType) },
+                    { "session_id", _sessionId }
                 })
             };
 
