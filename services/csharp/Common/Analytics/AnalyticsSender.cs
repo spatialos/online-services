77beb893275b8a680f8eea707efe7b5b73935cc3
Ashley Davies <ashleydavieslyons@improbable.io>
2019-08-06 15:42:20 +0100

    Add batching features to the AnalyticsSender (#62)
    * Add batching features to the AnalyticsSender
    
    * Add unit tests for the behaviour
    
    * Amend for comments and implement IDisposable

--- services/csharp/Common/Analytics/AnalyticsSender.cs
+++ services/csharp/Common/Analytics/AnalyticsSender.cs
@@ -1,8 +1,8 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
@@ -22,40 +22,80 @@ namespace Improbable.OnlineServices.Common.Analytics
 
     public class AnalyticsSender : IAnalyticsSender
     {
+        private struct QueuedRequest
+        {
+            public readonly Uri Uri;
+            public readonly string Content;
+
+            public QueuedRequest(Uri uri, string content)
+            {
+                Uri = uri;
+                Content = content;
+            }
+        }
+
         internal const string DefaultEventCategory = "cold";
 
         private readonly Uri _endpoint;
         private readonly AnalyticsConfig _config;
         private readonly AnalyticsEnvironment _environment;
+        private readonly CancellationTokenSource _timedDispatchCancelTokenSrc = new CancellationTokenSource();
         private readonly string _sessionId = Guid.NewGuid().ToString();
         private readonly string _gcpKey;
         private readonly string _eventSource;
+        private readonly int _maxEventQueueSize;
         private readonly HttpClient _httpClient;
-
+        private readonly ConcurrentQueue<QueuedRequest> _queuedRequests =
+            new ConcurrentQueue<QueuedRequest>();
         private long _eventId;
 
-        internal AnalyticsSender(Uri endpoint, AnalyticsConfig config, AnalyticsEnvironment environment,
-            string gcpKey, string eventSource, HttpClient httpClient)
+        private string CanonicalEnvironment => _environment.ToString().ToLower();
+        private bool IsQueueFull => _queuedRequests.Count >= _maxEventQueueSize;
+
+
+        internal AnalyticsSender(Uri endpoint, AnalyticsConfig config, AnalyticsEnvironment environment, string gcpKey,
+            string eventSource, TimeSpan maxEventQueueDelta, int maxEventQueueSize, HttpClient httpClient)
         {
             _endpoint = endpoint;
             _config = config;
             _environment = environment;
             _gcpKey = gcpKey;
             _eventSource = eventSource;
+            _maxEventQueueSize = maxEventQueueSize;
             _httpClient = httpClient;
+
+            Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    await DispatchEventQueue();
+                    await Task.Delay(maxEventQueueDelta, _timedDispatchCancelTokenSrc.Token);
+                }
+            }, _timedDispatchCancelTokenSrc.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
         }
 
         public async Task Send(string eventClass, string eventType, Dictionary<string, string> eventAttributes)
         {
             // Get previous event ID after an atomic increment
             var eventId = Interlocked.Increment(ref _eventId) - 1;
-            string environment = _environment.ToString().ToLower();
 
-            // TODO: Can the redundancy in postParams be fixed by amending the pipeline to import the URL
-            //   params into JSON?
-            var postParams = new Dictionary<string, string>
+            var postParams = PostParams(eventClass, eventType, eventAttributes, eventId);
+            var uri = RequestUri(eventClass, eventType);
+
+            _queuedRequests.Enqueue(new QueuedRequest(uri, JsonConvert.SerializeObject(postParams)));
+
+            if (IsQueueFull)
             {
-                { "eventEnvironment", environment },
+                await DispatchEventQueue();
+            }
+        }
+
+        private Dictionary<string, string> PostParams(string eventClass, string eventType,
+            Dictionary<string, string> eventAttributes, long eventId)
+        {
+            return new Dictionary<string, string>
+            {
+                { "eventEnvironment", CanonicalEnvironment },
                 { "eventIndex", eventId.ToString() },
                 { "eventSource", _eventSource },
                 { "eventClass", eventClass },
@@ -66,21 +106,38 @@ namespace Improbable.OnlineServices.Common.Analytics
                 { "eventTimestamp", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString() },
                 { "eventAttributes", JsonConvert.SerializeObject(eventAttributes) },
             };
+        }
 
-            UriBuilder builder = new UriBuilder(_endpoint)
+        private Uri RequestUri(string eventClass, string eventType)
+        {
+            return new UriBuilder(_endpoint)
             {
                 Query = DictionaryToQueryString(new Dictionary<string, string>
                 {
                     { "key", _gcpKey },
-                    { "analytics_environment", environment },
+                    { "analytics_environment", CanonicalEnvironment },
                     { "event_category", _config.GetCategory(eventClass, eventType) },
                     { "session_id", _sessionId }
                 })
-            };
+            }.Uri;
+        }
 
-            // TODO: Process response to handle failure / verify success
-            await _httpClient.PostAsync(builder.ToString(),
-                new StringContent(JsonConvert.SerializeObject(postParams)));
+        private async Task DispatchEventQueue()
+        {
+            Dictionary<Uri, List<string>> uriMap = new Dictionary<Uri, List<string>>();
+
+            while (_queuedRequests.TryDequeue(out QueuedRequest request))
+            {
+                if (!uriMap.ContainsKey(request.Uri))
+                {
+                    uriMap[request.Uri] = new List<string>();
+                }
+                uriMap[request.Uri].Add(request.Content);
+            }
+
+            var enumerable = uriMap.Select(
+                kvp => _httpClient.PostAsync(kvp.Key, new StringContent(string.Join("\n", kvp.Value))));
+            await Task.WhenAll(enumerable.ToArray<Task>());
         }
 
         private static string DictionaryToQueryString(Dictionary<string, string> urlParams)
@@ -88,6 +145,12 @@ namespace Improbable.OnlineServices.Common.Analytics
             return string.Join("&", urlParams.Select(
                 p => $"{HttpUtility.UrlEncode(p.Key)}={HttpUtility.UrlEncode(p.Value)}"
             ));
+        }
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+            _timedDispatchCancelTokenSrc.Cancel();
         }
     }
 }
