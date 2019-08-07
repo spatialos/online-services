53002a578f4fe797c5fa4edbcca753dff3265dc9
Loek Landzaat <loek@improbable.io>
2019-08-07 12:24:39 +0100

    updated dataflow versionId to 0.2.0 and removed venv-* from .gitignore

--- services/python/analytics-pipeline/src/dataflow/setup.py
+++ services/python/analytics-pipeline/src/dataflow/setup.py
@@ -69,7 +69,7 @@ REQUIRED_PACKAGES = []
 
 setuptools.setup(
     name='common',
-    version='0.1.0',
+    version='0.2.0',
     description='DataFlow Python Pipeline',
     install_requires=REQUIRED_PACKAGES,
     packages=setuptools.find_packages(),
