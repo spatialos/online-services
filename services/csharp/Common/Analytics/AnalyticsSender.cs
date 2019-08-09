f44384ce56c66b53edf67b2a93d78cb6e7519f93
Ashley Davies <ashleydavieslyons@improbable.io>
2019-08-09 15:04:34 +0100

    Rename buildVersion to versionId

--- services/csharp/Common/Analytics/AnalyticsSender.cs
+++ services/csharp/Common/Analytics/AnalyticsSender.cs
@@ -112,7 +112,7 @@ namespace Improbable.OnlineServices.Common.Analytics
                 { "eventType", eventType },
                 { "sessionId", _sessionId },
                 // TODO: Add versioning ability & resolve matching TODO in relevant unit tests
-                { "buildVersion", "0.2.0" },
+                { "versionId", "0.2.0" },
                 { "eventTimestamp", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString() },
                 { "eventAttributes", JsonConvert.SerializeObject(eventAttributes) },
             };
