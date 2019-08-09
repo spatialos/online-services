59aab18277d789b4adeae912100d345193716a28
Ashley Davies <ashleydavieslyons@improbable.io>
2019-08-09 16:28:52 +0100

    More pre-instrumentation changes to client-side Analytics API (#71)
    * Send multiple analytic events as a JSON array instead of
    newline-delimited, allow unknown arguments in Party, and
    non-case-sensitive enums
    
    * Named parameters for Enum.TryParse
    
    * Migrate to using interfaces for command line args
    
    * Make the Builder work with already parsed args to save re-parsing
    
    * Rename buildVersion to versionId

--- services/csharp/Common/Analytics/AnalyticsCommandLineArgs.cs
+++ services/csharp/Common/Analytics/AnalyticsCommandLineArgs.cs
@@ -2,7 +2,7 @@ using CommandLine;
 
 namespace Improbable.OnlineServices.Common.Analytics
 {
-    public class AnalyticsCommandLineArgs
+    public class AnalyticsCommandLineArgs : IAnalyticsCommandLineArgs
     {
         public const string EndpointName = "analytics.endpoint";
         public const string AllowInsecureEndpointName = "analytics.allow-insecure-endpoint";
@@ -10,23 +10,10 @@ namespace Improbable.OnlineServices.Common.Analytics
         public const string GcpKeyPathName = "analytics.gcp-key-path";
         public const string EnvironmentName = "analytics.environment";
 
-        [Option(EndpointName, HelpText = "Endpoint for analytics to be sent to. If not provided, then analytics " +
-                                         "are disabled")]
         public string Endpoint { get; set; }
-
-        [Option(AllowInsecureEndpointName, Default = false, HelpText = "If set, allows http URLs for the endpoint")]
         public bool AllowInsecureEndpoints { get; set; }
-
-        [Option(ConfigPathName, HelpText = "Set the path for the analytics configuration file. If not set, default " +
-                                           "values are assumed for all analytics events.")]
         public string ConfigPath { get; set; }
-
-        [Option(GcpKeyPathName, HelpText = "The path from which to load the GCP key for analytics reporting.")]
         public string GcpKeyPath { get; set; }
-
-        [Option(EnvironmentName,
-            HelpText = "Must be one of: testing, staging, production, live. Allows endpoint to route " +
-                       "analytics from different environments to different storage buckets.")]
         public string Environment { get; set; }
     }
 }
