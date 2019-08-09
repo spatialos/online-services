599b3d54f7f49ad40f8413a9c9971021ab42f129
Loek <loek+github@improbable.io>
2019-08-09 17:00:48 +0100

    Analytics python (#43)
    * added analytics endpoint & re-formatted terraform
    
    * Removing non-python
    
    * resolved first few comments PR
    
    * moved table schemas in separate file & renamed main.py entrypoint function
    
    * pep8 tweaks
    
    * changed environment variable names
    
    * PR tweaks
    
    * file names & functions now snake_case, class names now UpperCamelCase
    
    * changed table names
    
    * changed if not exists
    
    * various improvements
    
    * PR improvements
    
    * update environment variable naming
    
    * scale test nit
    
    * PR changes
    
    * various PR changes
    
    * moved dataflow imports up
    
    * PR changes
    
    * Endpoint now defaults to default value whenever parameter is passed but set to None, e.g. ..&event_category=&..
    
    * various PR tweaks
    
    * more python PR changes
    
    * python PR changes
    
    * changed environment variable names
    
    * changed env variable names
    
    * QA after PR changes
    
    * updated hash logic endpont
    
    * formatting tweaks & bq provision logic
    
    * more explicit mention of service account in main.py
    
    * nits endpoint code
    
    * PR nit
    
    * added defaults for v1/file if parameter set to None
    
    * PR nits
    
    * file_name to object_location
    
    * changed string formatting style for cloud function errors
    
    * fixed terraform files..
    
    * add back analytics-endpoint Dockerfile..
    
    * added back service #update pointers..
    
    * aligned version number
    
    * removed debug=True from app.run
    
    * various PR tweaks
    
    * .gitignore tweaks
    
    * venv specific to directory
    
    * project id -> Google project id
    
    * removed .gitignore in terraform/
    
    * updated dataflow versionId to 0.2.0 and removed venv-* from .gitignore
    
    * various PR tweaks
    
    * now catching ValueError
    
    * updated timestamp cast function & added debug comment to dataflow script
    
    * removed debug comments from dataflow script
    
    * aligned on a gspath / file_name / bucket_name / object_location naming convention x-python
    
    * various PR tweaks
    
    * fmt nits
    
    * various PR tweaks
    
    * added safe_convert_list_to_sql_tuple()
    
    * handling None..
    
    * relocated arg parser
    
    * PR tweasks
    
    * now writing & reading gzipped json
    
    * removed system calls from endpoint code, instead now part of ENTRYPOINT in Dockerfile
    
    * removed unnecessary isinstance() usage
    
    * final QA
    
    * nit: remove blank line
    
    * QA fix

---
+++ services/python/analytics-pipeline/src/dataflow/common/bigquery.py
@@ -0,0 +1,140 @@
+import re
+
+
+def generate_bigquery_assets(client_bq, bigquery_asset_list):
+
+    """ This function provisions all required BigQuery datasets & tables.
+    We combine datasets & tables within a single function as creating tables
+    requires the same references when creating datasets.
+    """
+
+    from common.bigquery_schema import bigquery_table_schema_dict
+    from google.cloud.exceptions import NotFound
+    from google.cloud import bigquery
+
+    def asset_exists(client, asset_type, reference):
+
+        if asset_type not in ['dataset', 'table']:
+            raise TypeError('Error - asset_type must be one of: {dataset, table}!')
+
+        try:
+            if asset_type == 'dataset':
+                client.get_dataset(reference)
+            elif asset_type == 'table':
+                client.get_table(reference)
+            return True
+        except NotFound:
+            return False
+
+    # Create dataset if it does not exist..
+    for dataset_name in set([bq_asset[0] for bq_asset in bigquery_asset_list]):
+        dataset_ref = client_bq.dataset(dataset_name)
+        if not asset_exists(client_bq, 'dataset', dataset_ref):
+            dataset = bigquery.Dataset(dataset_ref)
+            client_bq.create_dataset(dataset)
+
+    # Create table if it does not exist..
+    table_list = []
+    for bq_asset in bigquery_asset_list:
+        dataset_name, table_name, partition = bq_asset
+        table_ref = client_bq.dataset(dataset_name).table(table_name)
+        if not asset_exists(client_bq, 'table', table_ref):
+            table = bigquery.Table(table_ref, schema=bigquery_table_schema_dict[dataset_name])
+            if partition:
+                table.time_partitioning = bigquery.TimePartitioning(
+                  type_=bigquery.TimePartitioningType.DAY,
+                  field=partition)
+            table_list.append(client_bq.create_table(table))
+        else:
+            table_list.append(client_bq.get_table(table_ref))
+
+    return table_list
+
+
+def source_bigquery_assets(client_bq, bigquery_asset_list):
+
+    """ This function sources BigQuery assets. If this operation fails,
+    generate_bigquery_assets() should be invoked.
+    """
+
+    table_list = []
+    for bq_asset in bigquery_asset_list:
+        dataset_name, table_name, partition = bq_asset
+        dataset_ref = client_bq.dataset(dataset_name)
+        table_ref = dataset_ref.table(table_name)
+        table_list.append(client_bq.get_table(table_ref))
+
+    return table_list
+
+
+def generate_backfill_query(gcp, method, environment_tuple, category_tuple, ds_start, ds_stop, time_part_tuple, scale_test_name=''):
+
+    """ This function generates a SQL query used to verify which files are already ingested into native BigQuery storage.
+    Generally, the pipeline calling this function will omit these files from the total set of files it is tasked to ingest
+    into native BigQuery storage.
+
+    When you pass an argument as `--event-category=` or `--event-category=None` it will be None. However, it will be parsed as
+    an empty string. This means we assume the gspath in this case should match `.*/event_category=/.*`.
+    """
+
+    def extract_filter_tuple(sql_condition_list, name):
+        if name is None:
+            return "('')"
+        else:
+            return "{sql_condition_list}".format(sql_condition_list=sql_condition_list)
+
+    if None not in [ds_start, ds_stop]:
+        ds_filter = "BETWEEN '{ds_start}' AND '{ds_stop}'".format(ds_start=ds_start, ds_stop=ds_stop)
+    else:
+        ds_filter = "IN ('')"
+
+    if scale_test_name:
+        scale_test_logs_filter = "AND gspath LIKE '%{scale_test_name}%'".format(scale_test_name=scale_test_name)
+        scale_test_events_filter = "AND event_type = '{scale_test_name}'".format(scale_test_name=scale_test_name)
+    else:
+        scale_test_logs_filter, scale_test_events_filter = '', ''
+
+    query = """
+    SELECT DISTINCT
+      a.gspath
+    FROM
+        (
+        SELECT DISTINCT
+          gspath,
+          batch_id
+        FROM `{gcp}.logs.events_logs_{method}_native`
+        WHERE analytics_environment IN {environment_tuple}
+        AND event_ds {ds_filter}
+        AND event_time IN {time_part_tuple}
+        AND event_category IN {category_tuple}
+        {scale_test_logs_filter}
+        AND event = 'parse_initiated'
+        ) a
+    INNER JOIN
+        (
+        SELECT batch_id
+        FROM `{gcp}.events.events_{method}_native`
+        WHERE analytics_environment IN {environment_tuple}
+        {scale_test_events_filter}
+        UNION DISTINCT
+        SELECT batch_id
+        FROM `{gcp}.logs.events_debug_{method}_native`
+        WHERE analytics_environment IN {environment_tuple}
+        AND event_ds {ds_filter}
+        AND event_time IN {time_part_tuple}
+        AND event_category IN {category_tuple}
+        {scale_test_logs_filter}
+        ) b
+    ON a.batch_id = b.batch_id
+    ;
+    """.format(
+          gcp=gcp,
+          method=method,
+          environment_tuple=extract_filter_tuple(*environment_tuple),
+          category_tuple=extract_filter_tuple(*category_tuple),
+          ds_filter=ds_filter,
+          time_part_tuple=extract_filter_tuple(*time_part_tuple),
+          scale_test_logs_filter=scale_test_logs_filter,
+          scale_test_events_filter=scale_test_events_filter)
+
+    return re.sub(r'\n\s*\n', '\n', query, re.MULTILINE)
---
+++ services/python/analytics-pipeline/src/endpoint/common/classes.py
@@ -0,0 +1,60 @@
+import Crypto.Signature.PKCS1_v1_5 as PKCS1_v1_5
+import Crypto.Hash.SHA256 as SHA256
+import datetime
+import requests
+import base64
+import time
+
+
+class CloudStorageURLSigner(object):
+
+    """ Contains methods for generating signed URLs for Google Cloud Storage.
+    """
+
+    def __init__(self, key, client_id_email, expiration=None, session=None):
+        self.key = key
+        self.client_id_email = client_id_email
+        self.gcs_api_endpoint = 'https://storage.googleapis.com'
+
+        self.expiration = expiration or (datetime.datetime.now() + datetime.timedelta(minutes=30))
+        self.expiration = int(time.mktime(self.expiration.timetuple()))
+
+    def base64_sign(self, plaintext):
+
+        """ Signs and returns a base64-encoded SHA256 digest.
+        """
+
+        shahash = SHA256.new(plaintext.encode('utf-8'))
+        signer = PKCS1_v1_5.new(self.key)
+        signature_bytes = signer.sign(shahash)
+        return base64.b64encode(signature_bytes)
+
+    def make_signature_string(self, verb, path, content_md5, content_type):
+
+        """ Creates the signature string for signing according to GCS docs.
+        """
+
+        signature_string = ('{verb}\n'
+                            '{content_md5}\n'
+                            '{content_type}\n'
+                            '{expiration}\n'
+                            '{resource}')
+        return signature_string.format(verb=verb, content_md5=content_md5,
+          content_type=content_type, expiration=self.expiration, resource=path)
+
+    def make_url(self, verb, path, content_type='', content_md5=''):
+
+        """ Forms and returns the full signed URL to access GCS.
+        """
+
+        base_url = '%s%s' % (self.gcs_api_endpoint, path)
+        signature_string = self.make_signature_string(verb=verb, path=path, content_md5=content_md5, content_type=content_type)
+        signature_signed = self.base64_sign(signature_string)
+        query_params = {'GoogleAccessId': self.client_id_email, 'Expires': str(self.expiration), 'Signature': signature_signed}
+        return base_url, query_params
+
+    def put(self, path, content_type, md5_digest):
+        base_url, query_params = self.make_url(verb='PUT', path=path, content_type=content_type, content_md5=md5_digest)
+        headers = {'Content-Type': content_type, 'Content-MD5': md5_digest}
+        request = requests.Request('PUT', base_url, params=query_params).prepare()
+        return {'signed_url': request.url, 'headers': headers, 'md5_digest': md5_digest, 'code': 200}
