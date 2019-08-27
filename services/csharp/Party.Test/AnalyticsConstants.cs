4596ecd0d9c947d362d1202cf156334e44d03db1
Ashley Davies <ashleydavieslyons@improbable.io>
2019-08-27 11:22:56 +0100

    Analytics unit tests (#77)
    * Add unit tests to verify analytics behaviour

---
+++ services/csharp/Party.Test/AnalyticsConstants.cs
@@ -0,0 +1,18 @@
+namespace Party.Test
+{
+    /// <summary>
+    /// Constant key strings etc. which are reused numerous times across analytics-related testing
+    /// </summary>
+    public class AnalyticsConstants
+    {
+        public const string InviteClass = "gateway_invite";
+        public const string PartyClass = "gateway_party";
+
+        public const string Invites = "invites";
+        public const string InviteId = "inviteId";
+        public const string PartyId = "partyId";
+        public const string PlayerId = "playerId";
+        public const string PlayerIdInviter = "playerIdInviter";
+        public const string PlayerIdKicker = "playerIdKicker";
+    }
+}
