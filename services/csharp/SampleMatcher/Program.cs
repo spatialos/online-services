f99b5559f2943223f4ab8a4e997db98b3ec088a9
Loek Landzaat <loek@improbable.io>
2019-09-18 17:53:47 +0100

    instrumented sample matcher

--- services/csharp/SampleMatcher/Program.cs
+++ services/csharp/SampleMatcher/Program.cs
@@ -1,41 +1,71 @@
 ﻿using System;
 using System.Threading.Tasks;
+using CommandLine;
+using Improbable.OnlineServices.Base.Server;
+using Improbable.OnlineServices.Common.Analytics;
+using Improbable.OnlineServices.Common.Analytics.ExceptionHandlers;
 using Mono.Unix;
 using Mono.Unix.Native;
+using Serilog;
+using Serilog.Formatting.Compact;
 
 namespace Improbable.OnlineServices.SampleMatcher
 {
+    class MatcherArgs : CommandLineArgs, IAnalyticsCommandLineArgs
+    {
+        public string Endpoint { get; set; }
+        public bool AllowInsecureEndpoints { get; set; }
+        public string ConfigPath { get; set; }
+        public string GcpKeyPath { get; set; }
+        public string Environment { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var matcher = new Matcher();
-            var matcherTask = new Task(() => { matcher.Start(); });
-            var unixSignalTask = new Task<int>(() =>
-            {
-                return UnixSignal.WaitAny(new[] { new UnixSignal(Signum.SIGINT), new UnixSignal(Signum.SIGTERM) });
-            });
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(new RenderedCompactJsonFormatter())
+                .Enrich.FromLogContext()
+                .CreateLogger();
 
-            matcherTask.Start();
-            Console.WriteLine("Matcher started up");
-            unixSignalTask.Start();
+            Parser.Default.ParseArguments<MatcherArgs>(args)
+                .WithParsed(parsedArgs =>
+                {
+                    IAnalyticsSender analyticsSender = new AnalyticsSenderBuilder("gateway_matcher")
+                        .WithCommandLineArgs(parsedArgs)
+                        .With(new LogExceptionStrategy(Log.Logger))
+                        .Build();
 
-            Task.WaitAny(matcherTask, unixSignalTask);
-            if (unixSignalTask.IsCompleted)
-            {
-                Console.WriteLine($"Received UNIX signal {unixSignalTask.Result}");
-                Console.WriteLine("Matcher shutting down...");
-                matcher.Stop();
-                matcherTask.Wait(TimeSpan.FromSeconds(10));
-                Console.WriteLine("Matcher stopped cleanly");
-            }
-            else
-            {
-                /* The matcher task has completed; we can just exit. */
-                Console.WriteLine("The matcher has stopped itself or encountered an unhandled exception.");
-            }
+                    var matcher = new Matcher(analyticsSender);
+                    var matcherTask = new Task(() => { matcher.Start(); });
+                    var unixSignalTask = new Task<int>(() =>
+                    {
+                        return UnixSignal.WaitAny(new[]
+                            {new UnixSignal(Signum.SIGINT), new UnixSignal(Signum.SIGTERM)});
+                    });
 
-            Environment.Exit(0);
+                    matcherTask.Start();
+                    Console.WriteLine("Matcher started up");
+                    unixSignalTask.Start();
+
+                    Task.WaitAny(matcherTask, unixSignalTask);
+                    if (unixSignalTask.IsCompleted)
+                    {
+                        Console.WriteLine($"Received UNIX signal {unixSignalTask.Result}");
+                        Console.WriteLine("Matcher shutting down...");
+                        matcher.Stop();
+                        matcherTask.Wait(TimeSpan.FromSeconds(10));
+                        Console.WriteLine("Matcher stopped cleanly");
+                    }
+                    else
+                    {
+                        /* The matcher task has completed; we can just exit. */
+                        Console.WriteLine("The matcher has stopped itself or encountered an unhandled exception.");
+                    }
+
+                    Environment.Exit(0);
+                });
         }
     }
 }
