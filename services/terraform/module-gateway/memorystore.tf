eb09890ba2bbc40c1b1784acb18fc4fc2053512c
Loek Landzaat <loek@improbable.io>
2019-09-18 18:09:32 +0100

    removed OS table & renamed memory store back to original

--- services/terraform/module-gateway/memorystore.tf
+++ services/terraform/module-gateway/memorystore.tf
@@ -1,7 +1,7 @@
 # This file defines the MemoryStore instance the Gateway uses as its queue.
 
 resource "google_redis_instance" "queue" {
-    name           = "${var.k8s_cluster_name}-queue-v2"
+    name           = "${var.k8s_cluster_name}-queue"
     location_id    = "${var.gcloud_zone}"
     memory_size_gb = 1
 }
