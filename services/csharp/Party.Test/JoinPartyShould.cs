8b1dc47884a308efd327e7ddd88c477513aef6b5
Loek <loek+github@improbable.io>
2019-09-20 13:38:50 +0100

    k8s yaml changes + addition of config file for instrumentation (#96)
    * Instrument party and invite services (#74)
    
    * Instrument party and invite services
    
    * Parser.Default
    
    * Regionify
    
    * Remove dependency on CSharpx
    
    * Add updated event
    
    * k8s yaml changes + addition of config file for instrumentation
    
    * deployment pool default back from 1 to 3
    
    * upgraded ESP container version for PlayFab auth
    
    * PR fixes
    
    * revert whitespace trim of comments
    
    * revert trim on comment
    
    * fixed formatting error in AnalyticsSender.cs

--- services/csharp/Party.Test/JoinPartyShould.cs
+++ services/csharp/Party.Test/JoinPartyShould.cs
@@ -77,7 +77,7 @@ namespace Party.Test
         [Test]
         public void ReturnAlreadyExistsWhenThePlayerIsAMemberOfAnotherParty()
         {
-            // Setup the client such that it will claim that the player is already a member of another party. 
+            // Setup the client such that it will claim that the player is already a member of another party.
             _mockMemoryStoreClient.Setup(client => client.GetAsync<PartyDataModel>(_partyToJoin.Id)).ReturnsAsync(_partyToJoin);
             _mockMemoryStoreClient.Setup(client => client.GetAsync<Member>(TestPlayerId))
                 .ReturnsAsync(new Member(TestPlayerId, "AnotherPartyId"));
