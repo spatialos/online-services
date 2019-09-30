8f0383d2991557e6603fd5f16527cc49bb273d77
Boyan Asenov <boyan@improbable.io>
2019-09-30 16:51:08 +0100

    Fix test

--- services/csharp/IntegrationTest/MatchmakingSystemShould.cs
+++ services/csharp/IntegrationTest/MatchmakingSystemShould.cs
@@ -105,24 +105,6 @@ namespace IntegrationTest
                     CallSettings.FromHeader(PitRequestHeaderName, _leaderPit)));
             Assert.AreEqual(StatusCode.NotFound, rpcException.Status.StatusCode);
 
-            // Join matchmaking.
-            var opSecond = _gatewayClient.Join(new JoinRequest
-            {
-                MatchmakingType = "no_match"
-            }, _leaderMetadata);
-            Assert.AreEqual(LeaderPlayerId, opSecond.Name);
-            Assert.False(opSecond.Done);
-
-            // Verify that the party has not been matched yet. 
-            fetchedOp = _operationsClient.GetOperation(LeaderPlayerId,
-                CallSettings.FromHeader(PitRequestHeaderName, _leaderPit));
-            Assert.AreEqual(LeaderPlayerId, fetchedOp.Name);
-            Assert.False(fetchedOp.Done);
-
-            // Cancel matchmaking.
-            _operationsClient.DeleteOperation(LeaderPlayerId,
-                CallSettings.FromHeader(PitRequestHeaderName, _leaderPit));
-
             // Clean-up.
             _partyClient.DeleteParty(new DeletePartyRequest(), _leaderMetadata);
         }
@@ -164,6 +146,24 @@ namespace IntegrationTest
             Assert.NotNull(inviteAnotherPlayer);
             _partyClient.JoinParty(new JoinPartyRequest { PartyId = partyId },
                 new Metadata { { PitRequestHeaderName, pitAnotherMember } });
+            
+            // Join matchmaking.
+            var opSecond = _gatewayClient.Join(new JoinRequest
+            {
+                MatchmakingType = "no_match"
+            }, _leaderMetadata);
+            Assert.AreEqual(LeaderPlayerId, opSecond.Name);
+            Assert.False(opSecond.Done);
+
+            // Verify that the party has not been matched yet. 
+            fetchedOp = _operationsClient.GetOperation(LeaderPlayerId,
+                CallSettings.FromHeader(PitRequestHeaderName, _leaderPit));
+            Assert.AreEqual(LeaderPlayerId, fetchedOp.Name);
+            Assert.False(fetchedOp.Done);
+
+            // Cancel matchmaking.
+            _operationsClient.DeleteOperation(LeaderPlayerId,
+                CallSettings.FromHeader(PitRequestHeaderName, _leaderPit));
 
             // Clean-up.
             _partyClient.DeleteParty(new DeletePartyRequest(), _leaderMetadata);
