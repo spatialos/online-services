3837bfce75a13497fd017515d08fd7e918f1fb37
Joel Auterson <joel@improbable.io>
2019-10-15 17:56:46 +0100

    reinstate metadata secret in integ test

--- ci/integration-tests.sh
+++ ci/integration-tests.sh
@@ -16,6 +16,7 @@ export SPATIAL_REFRESH_TOKEN
 SPATIAL_REFRESH_TOKEN=$(imp-ci secrets read --environment=production --buildkite-org=improbable --secret-type=spatialos-service-account --secret-name="online-services-test" | jq -Mr '.token')
 export PLAYFAB_SECRET_KEY
 PLAYFAB_SECRET_KEY=$(imp-ci secrets read --environment=production --buildkite-org=improbable --secret-type=playfab-secret-key --secret-name="online-services-playfab-secret-key-integ" | jq -Mr '.token')
+export DEPLOYMENT_METADATA_SERVER_SECRET="test_secret"
 export TEST_RESULTS_DIR
 export COMPOSE_NETWORK_SUFFIX="${BUILDKITE_JOB_ID}"
 sh test.sh --test_all
