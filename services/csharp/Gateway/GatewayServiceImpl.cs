8e4c3a0286e320b4fa16cd7ce619f6fb10bf7614
Joel Auterson <joel@improbable.io>
2019-10-28 16:01:15 +0000

    Instrument CancelJoin RPC

--- services/csharp/Gateway/GatewayServiceImpl.cs
+++ services/csharp/Gateway/GatewayServiceImpl.cs
@@ -229,6 +229,27 @@ namespace Gateway
                     }
 
                     Reporter.CancelJoinInc();
+
+                    var eventAttributes = new Dictionary<string, string>
+                    {
+                        { "partyId", partyJoinRequest.Id },
+                        { "matchRequestId", partyJoinRequest.MatchRequestId },
+                        { "queueType", partyJoinRequest.Type }
+                    };
+                    _analytics.Send("player_cancels_match_request", eventAttributes, partyJoinRequest.Party.LeaderPlayerId);
+                    var eventAttributesParty = new Dictionary<string, string>(eventAttributes) { { "partyPhase", "Forming" } };
+                    _analytics.Send("party_match_request_cancelled", eventAttributesParty, partyJoinRequest.Party.LeaderPlayerId);
+
+                    foreach (var playerJoinRequest in toDelete.OfType<PlayerJoinRequest>())
+                    {
+                        _analytics.Send("player_left_cancelled_match_request", new Dictionary<string, string>
+                        {
+                            { "partyId", playerJoinRequest.PartyId },
+                            { "matchRequestId", playerJoinRequest.MatchRequestId },
+                            { "queueType", playerJoinRequest.Type }
+                        }, playerJoinRequest.Id);
+                    }
+
                     return new CancelJoinResponse();
                 }
                 catch (EntryNotFoundException exception)
