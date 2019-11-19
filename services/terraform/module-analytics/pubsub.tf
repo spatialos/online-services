539896b289c154012150fc1fbd25a99714bdf53f
Loek Landzaat <loek@improbable.io>
2019-11-19 10:51:54 +0000

    fix missing dependency in gcs notification terraform file

--- services/terraform/module-analytics/pubsub.tf
+++ services/terraform/module-analytics/pubsub.tf
@@ -19,56 +19,81 @@ resource "google_pubsub_topic_iam_member" "member_cloud_function" {
 # Create GCS to Pub/Sub Topic Notifications.
 
 resource "google_storage_notification" "notification_function_development" {
-    bucket             = "${var.gcloud_project}-analytics"
-    payload_format     = "JSON_API_V1"
-    topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
-    # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
-    event_types        = ["OBJECT_FINALIZE"]
-    # Only trigger a message to Pub/Sub for files hitting this prefix:
-    object_name_prefix = "data_type=json/analytics_environment=development/event_category=function/"
-    depends_on         = ["google_pubsub_topic_iam_member.member_cloud_function"]
+
+  depends_on = [
+    "google_pubsub_topic_iam_member.member_cloud_function",
+    "google_storage_bucket.analytics_bucket"
+  ]
+
+  bucket             = "${var.gcloud_project}-analytics"
+  payload_format     = "JSON_API_V1"
+  topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
+  # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
+  event_types        = ["OBJECT_FINALIZE"]
+  # Only trigger a message to Pub/Sub for files hitting this prefix:
+  object_name_prefix = "data_type=json/analytics_environment=development/event_category=function/"
 }
 
 resource "google_storage_notification" "notification_function_testing" {
-    bucket             = "${var.gcloud_project}-analytics"
-    payload_format     = "JSON_API_V1"
-    topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
-    # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
-    event_types        = ["OBJECT_FINALIZE"]
-    # Only trigger a message to Pub/Sub for files hitting this prefix:
-    object_name_prefix = "data_type=json/analytics_environment=testing/event_category=function/"
-    depends_on         = ["google_pubsub_topic_iam_member.member_cloud_function"]
+
+  depends_on = [
+    "google_pubsub_topic_iam_member.member_cloud_function",
+    "google_storage_bucket.analytics_bucket"
+  ]
+
+  bucket             = "${var.gcloud_project}-analytics"
+  payload_format     = "JSON_API_V1"
+  topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
+  # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
+  event_types        = ["OBJECT_FINALIZE"]
+  # Only trigger a message to Pub/Sub for files hitting this prefix:
+  object_name_prefix = "data_type=json/analytics_environment=testing/event_category=function/"
 }
 
 resource "google_storage_notification" "notification_function_staging" {
-    bucket             = "${var.gcloud_project}-analytics"
-    payload_format     = "JSON_API_V1"
-    topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
-    # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
-    event_types        = ["OBJECT_FINALIZE"]
-    # Only trigger a message to Pub/Sub for files hitting this prefix:
-    object_name_prefix = "data_type=json/analytics_environment=staging/event_category=function/"
-    depends_on         = ["google_pubsub_topic_iam_member.member_cloud_function"]
+
+  depends_on = [
+    "google_pubsub_topic_iam_member.member_cloud_function",
+    "google_storage_bucket.analytics_bucket"
+  ]
+
+  bucket             = "${var.gcloud_project}-analytics"
+  payload_format     = "JSON_API_V1"
+  topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
+  # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
+  event_types        = ["OBJECT_FINALIZE"]
+  # Only trigger a message to Pub/Sub for files hitting this prefix:
+  object_name_prefix = "data_type=json/analytics_environment=staging/event_category=function/"
 }
 
 resource "google_storage_notification" "notification_function_production" {
-    bucket             = "${var.gcloud_project}-analytics"
-    payload_format     = "JSON_API_V1"
-    topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
-    # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
-    event_types        = ["OBJECT_FINALIZE"]
-    # Only trigger a message to Pub/Sub for files hitting this prefix:
-    object_name_prefix = "data_type=json/analytics_environment=production/event_category=function/"
-    depends_on         = ["google_pubsub_topic_iam_member.member_cloud_function"]
+
+  depends_on = [
+    "google_pubsub_topic_iam_member.member_cloud_function",
+    "google_storage_bucket.analytics_bucket"
+  ]
+
+  bucket             = "${var.gcloud_project}-analytics"
+  payload_format     = "JSON_API_V1"
+  topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
+  # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
+  event_types        = ["OBJECT_FINALIZE"]
+  # Only trigger a message to Pub/Sub for files hitting this prefix:
+  object_name_prefix = "data_type=json/analytics_environment=production/event_category=function/"
 }
 
 resource "google_storage_notification" "notification_function_live" {
-    bucket             = "${var.gcloud_project}-analytics"
-    payload_format     = "JSON_API_V1"
-    topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
-    # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
-    event_types        = ["OBJECT_FINALIZE"]
-    # Only trigger a message to Pub/Sub for files hitting this prefix:
-    object_name_prefix = "data_type=json/analytics_environment=live/event_category=function/"
-    depends_on         = ["google_pubsub_topic_iam_member.member_cloud_function"]
+
+  depends_on = [
+    "google_pubsub_topic_iam_member.member_cloud_function",
+    "google_storage_bucket.analytics_bucket"
+  ]
+
+  bucket             = "${var.gcloud_project}-analytics"
+  payload_format     = "JSON_API_V1"
+  topic              = "${google_pubsub_topic.cloud_function_gcs_to_bq_topic.id}"
+  # See other event_types here: https://cloud.google.com/storage/docs/pubsub-notifications#events
+  event_types        = ["OBJECT_FINALIZE"]
+  # Only trigger a message to Pub/Sub for files hitting this prefix:
+  object_name_prefix = "data_type=json/analytics_environment=live/event_category=function/"
 }
