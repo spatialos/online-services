c679fb8ca9ece2cc2cb6fe1967196efb9ecd2a2b
Loek Landzaat <loek@improbable.io>
2019-12-16 18:02:04 +0000

    added second cloud function to Terraform config

--- services/terraform/module-analytics/archive.tf
+++ services/terraform/module-analytics/archive.tf
@@ -1,11 +1,11 @@
 # This file creates a .zip file with everything required to run our Cloud Function.
 
-data "archive_file" "cloud_function_analytics" {
+data "archive_file" "cloud_function_general_schema" {
   type        = "zip"
-  output_path = "${path.module}/../../python/analytics-pipeline/cloud-function-analytics.zip"
+  output_path = "${path.module}/../../python/analytics-pipeline/cloud-function-general-schema.zip"
 
   source {
-    content  = "${file("${path.module}/../../python/analytics-pipeline/src/function/main.py")}"
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/functions/general/main.py")}"
     filename = "main.py"
   }
 
@@ -33,5 +33,39 @@ data "archive_file" "cloud_function_analytics" {
     content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/functions.py")}"
     filename = "common/functions.py"
   }
+}
 
+data "archive_file" "cloud_function_playfab_schema" {
+  type        = "zip"
+  output_path = "${path.module}/../../python/analytics-pipeline/cloud-function-playfab-schema.zip"
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/functions/playfab/main.py")}"
+    filename = "main.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/requirements/function.txt")}"
+    filename = "requirements.txt"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/__init__.py")}"
+    filename = "common/__init__.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/bigquery.py")}"
+    filename = "common/bigquery.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/bigquery_schema.py")}"
+    filename = "common/bigquery_schema.py"
+  }
+
+  source {
+    content  = "${file("${path.module}/../../python/analytics-pipeline/src/dataflow/common/functions.py")}"
+    filename = "common/functions.py"
+  }
 }
--- services/terraform/module-analytics/cloud-function.tf
+++ services/terraform/module-analytics/cloud-function.tf
@@ -1,30 +1,43 @@
 # This file stores our .zip file in GCS & subsequently deploys the Cloud Function.
 
-# Store the .zip file in GCS.
-resource "google_storage_bucket_object" "function_analytics" {
-  name   = "analytics/function-gcs-to-bq-${random_pet.cloud_function_pet.id}.zip"
+# Store the .zip files in GCS.
+resource "google_storage_bucket_object" "function_general_schema" {
+  name   = "analytics/function-general-schema-${random_pet.function_general_schema.id}.zip"
   bucket = google_storage_bucket.functions_bucket.name
-  source = "${path.module}/../../python/analytics-pipeline/cloud-function-analytics.zip"
+  source = "${path.module}/../../python/analytics-pipeline/cloud-function-general-schema.zip"
+}
+
+resource "google_storage_bucket_object" "function_playfab_schema" {
+  name   = "analytics/function-playfab-schema-${random_pet.function_playfab_schema.id}.zip"
+  bucket = google_storage_bucket.functions_bucket.name
+  source = "${path.module}/../../python/analytics-pipeline/cloud-function-playfab-schema.zip"
 }
 
 # We attach a random pet name to the name of our cloud function to force a refresh
 # whenever the source code changes.
-resource "random_pet" "cloud_function_pet" {
+resource "random_pet" "function_general_schema" {
   length  = 1
   keepers = {
-    file_hash = data.archive_file.cloud_function_analytics.output_md5
+    file_hash = data.archive_file.cloud_function_general_schema.output_md5
   }
 }
 
-# Deploy the Cloud Function.
-resource "google_cloudfunctions_function" "function_analytics" {
-  name                  = "function-gcs-to-bq-${random_pet.cloud_function_pet.id}"
+resource "random_pet" "function_playfab_schema" {
+  length  = 1
+  keepers = {
+    file_hash = data.archive_file.cloud_function_playfab_schema.output_md5
+  }
+}
+
+# Deploy the Cloud Functions.
+resource "google_cloudfunctions_function" "function_general_schema" {
+  name                  = "function-general-schema-${random_pet.function_general_schema.id}"
   description           = "GCS to Native BigQuery Cloud Function"
   runtime               = "python37"
 
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.functions_bucket.name
-  source_archive_object = google_storage_bucket_object.function_analytics.name
+  source_archive_object = google_storage_bucket_object.function_general_schema.name
   timeout               = 180
   # The name of the Python function to invoke in ../../python/function/main.py:
   entry_point           = "ingest_into_native_bigquery_storage"
@@ -32,7 +45,30 @@ resource "google_cloudfunctions_function" "function_analytics" {
 
   event_trigger {
     event_type = "google.pubsub.topic.publish"
-    resource   = google_pubsub_topic.cloud_function_gcs_to_bq_topic.name
+    resource   = google_pubsub_topic.cloud_function_general_schema.name
+  }
+
+  environment_variables = {
+    LOCATION = var.cloud_storage_location
+  }
+}
+
+resource "google_cloudfunctions_function" "function_playfab_schema" {
+  name                  = "function-playfab-schema-${random_pet.function_general_schema.id}"
+  description           = "GCS to Native BigQuery Cloud Function"
+  runtime               = "python37"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.functions_bucket.name
+  source_archive_object = google_storage_bucket_object.function_playfab_schema.name
+  timeout               = 180
+  # The name of the Python function to invoke in ../../python/function/main.py:
+  entry_point           = "ingest_into_native_bigquery_storage"
+  service_account_email = google_service_account.cloud_function_gcs_to_bq.email
+
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.cloud_function_playfab_schema.name
   }
 
   environment_variables = {
